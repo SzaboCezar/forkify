70e15ab9fa4af016f242202d5306c05822784dca
SzaboCezar <szabocezar@yahoo.com>
2023-10-19 14:35:51 +0300

    Some minor changes in helpers and config

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,5 @@
 import { async } from "regenerator-runtime";
+import { TIMEOUT_SEC } from "./config.js";
 
 const timeout = function (s) {
     return new Promise(function (_, reject) {
@@ -10,11 +11,10 @@ const timeout = function (s) {
 
 export const getJSON = async function (url) {
     try {
-        const response = await Promise.race([fetch(url), timeout(0.5)]);
+        const response = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
         const data = await response.json();
         if (!response.ok) throw new Error(`${response.statusText} (${response.status})`);
         return data;
-
     } catch (err) {
         throw err;
     }
